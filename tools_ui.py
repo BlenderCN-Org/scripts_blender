094371352731c3a618dee3e6b6375940fea2fcfa
paulocsilvajr <pauluscave@gmail.com>
2017-07-16 12:36:28 -0300

    Corrigido docstring e adicionado novas funções

--- tools_ui.py
+++ tools_ui.py
@@ -32,12 +32,18 @@ class ToolsPanel(bpy.types.Panel):
         row.operator("object.bloquear_localizacao")
 
         row = layout.row()
+        row.operator("object.bloquear_rotacao")
+
+        row = layout.row()
+        row.operator("object.bloquear_escala")
+
+        row = layout.row()
         row.operator("object.teste")
 
 
 class RemoverModificadores(bpy.types.Operator):
-    """ Operator 'Remover modificadores'
-    ref. a função remover_all_modifiers() """
+    """ Operator 'Remover modificadores'. Remove todos os modificadores de todos os objetos MESH da cena. """
+    # ref. a função remover_all_modifiers()
     bl_idname = "object.remover_modificadores"
     bl_label = "Remover modificadores"
 
@@ -51,8 +57,8 @@ class RemoverModificadores(bpy.types.Operator):
 
 
 class BloquearLocalizacao(bpy.types.Operator):
-    """ Operator 'Bloquear localização'
-    ref. a função lock_location() """
+    """ Operator 'Bloquear localização'. Bloqueia todos os eixos de localização do objeto ativo. """
+    # Ref. a função lock()
     bl_idname = "object.bloquear_localizacao"
     bl_label = "Bloquear localização"
 
@@ -61,13 +67,43 @@ class BloquearLocalizacao(bpy.types.Operator):
         return context.active_object is not None
 
     def execute(self, context):
-        lock_location(context)
+        lock(context, 'location')
+        return {'FINISHED'}
+
+
+class BloquearRotacao(bpy.types.Operator):
+    """ Operator 'Bloquear rotação'. Bloqueia todos os eixos de rotação do objeto ativo. """
+    # Ref. a função lock()
+    bl_idname = "object.bloquear_rotacao"
+    bl_label = "Bloquear rotação"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        lock(context, 'rotation')
+        return {'FINISHED'}
+
+
+class BloquearEscala(bpy.types.Operator):
+    """ Operator 'Bloquear escala'. Bloqueia todos os eixos de escala do objeto ativo. """
+    # Ref. a função lock_location()
+    bl_idname = "object.bloquear_escala"
+    bl_label = "Bloquear escala"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        lock(context, 'scale')
         return {'FINISHED'}
 
 
 class Teste(bpy.types.Operator):
-    """ Operator 'teste'
-    ref. a função teste() """
+    """ Operator 'teste'. Teste de funções. """
+    # ref. a função teste()
     bl_idname = "object.teste"
     bl_label = "Teste"
 
@@ -111,24 +147,51 @@ def remove_all_modifiers(context, obj_type='MESH'):
 def teste(context):
     objeto_ativo = context.active_object
     print("Objeto ativo: {}".format(objeto_ativo))
-    print(dir(objeto_ativo))
+    # print(dir(objeto_ativo))
+
+    # os valores de rotação então en radianos,
+    # usar a sintaxe abaixo para converter em angulo
+    # from math import radians
+    # angulo = randians(objeto_ativo.rotation_euler[0])
+    #
+    # location e scale estão em vector, usar
+    # from mathutils import Vector
+    # objeto_ativo.location += Vector((4, 0, 0))  # para incrementar
+    print(objeto_ativo.location,
+          objeto_ativo.rotation_euler,
+          objeto_ativo.scale,
+          sep='\n')
 
 
-def lock_location(context):
+def lock(context, type_operation):
     obj = context.active_object
-    lock = not obj.lock_location[0]
+    if type_operation == 'location':
+        tipo = 'localizacao'
+        operacao = obj.lock_location
+    elif type_operation == 'rotation':
+        tipo = 'rotação'
+        operacao = obj.lock_rotation
+    elif type_operation == 'scale':
+        tipo = 'escala'
+        operacao = obj.lock_scale
+    else:
+        print('Tipo de operação inválida: {}'.format(type_operation))
+
+    lock = not operacao[0]
     for i in range(3):
-        context.active_object.lock_location[i] = lock
+        operacao[i] = lock
 
-    condicao = "Bloqueado" if lock else "Desbloqueado"
+    condicao = "Bloqueado " if lock else "Desbloqueado "
 
-    impressao_formatada(condicao + " localizaçao de {}", obj.name)
+    impressao_formatada(condicao + tipo + " de {}", obj.name)
 
 
 # Registradores do blender para as classes de UI e Operators
 def register():
     bpy.utils.register_class(RemoverModificadores)
     bpy.utils.register_class(BloquearLocalizacao)
+    bpy.utils.register_class(BloquearRotacao)
+    bpy.utils.register_class(BloquearEscala)
     bpy.utils.register_class(Teste)
     bpy.utils.register_class(ToolsPanel)
 
@@ -136,6 +199,8 @@ def register():
 def unregister():
     bpy.utils.unregister_class(RemoverModificadores)
     bpy.utils.unregister_class(BloquearLocalizacao)
+    bpy.utils.unregister_class(BloquearRotacao)
+    bpy.utils.unregister_class(BloquearEscala)
     bpy.utils.unregister_class(Teste)
     bpy.utils.unregister_class(ToolsPanel)
 
