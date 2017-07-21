6ba4c6bccb1e5268badf442cc4a646172a65fce4
paulocsilvajr <pauluscave@gmail.com>
2017-07-21 08:32:34 -0300

    Adicionado propriedades smooth e flat

--- tools_ui.py
+++ tools_ui.py
@@ -49,6 +49,23 @@ class ToolsPanel(bpy.types.Panel):
         row.prop(obj, "color", text="Cor")
         row.prop(obj, "active_material", text="Material")
 
+        # SMOOTH, FLAT
+        row = layout.row(align=True)
+        row.operator("object.shade_smooth", text="Smooth")
+        row.operator("object.shade_flat", text="Flat")
+
+        mesh = obj.data  # captura do mesh do ativo
+
+        split = layout.box().split()
+
+        col = split.column()
+        col.prop(mesh, "use_auto_smooth")
+        sub = col.column()
+        sub.active = mesh.use_auto_smooth and not mesh.has_custom_normals
+        sub.prop(mesh, "auto_smooth_angle", text="Angle")
+
+        split.prop(mesh, "show_double_sided")
+
         row = layout.row()
         row.prop(obj, "parent", text="Pai")
 
